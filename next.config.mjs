882363c032b51a513e22d140fe4aa2c9f66a9f23
cifroluma <takhmazyanlab@ro.ru>
2026-07-28 17:11:20 +0300

    Configure custom domain

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,8 @@
 const repository = process.env.GITHUB_REPOSITORY?.split('/')[1] ?? '';
 const owner = process.env.GITHUB_REPOSITORY?.split('/')[0] ?? '';
 const isUserSite = repository === `${owner}.github.io`;
-const basePath = process.env.GITHUB_ACTIONS === 'true' && !isUserSite ? `/${repository}` : '';
+const isCustomDomain = process.env.NEXT_PUBLIC_CUSTOM_DOMAIN === 'true';
+const basePath = process.env.GITHUB_ACTIONS === 'true' && !isUserSite && !isCustomDomain ? `/${repository}` : '';
 
 const nextConfig = {
   output: 'export',
